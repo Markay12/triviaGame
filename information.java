27143fc60d969f1f20d24a9b426bfdcef33ced1f
Markay12 <54555076+Markay12@users.noreply.github.com>
2019-12-15 00:30:25 -0700

    information about the user

---
+++ information.java
@@ -0,0 +1,23 @@
+public class information{
+    
+    private static String name;
+
+    public information(){
+
+        name = this.name;
+
+    }
+
+    public void setName(String inputName){
+
+        name = inputName;
+
+    }
+
+    public String getName(){
+
+        return name;
+
+    }
+
+}
