3a1b5c58850b809fc42937895835b3617c2b9533
Markay12 <54555076+Markay12@users.noreply.github.com>
2019-12-15 20:00:08 -0700

    Added math questions and methods

--- Main.java
+++ Main.java
@@ -3,10 +3,12 @@ import java.util.*;
 public class Main{
     public static void main(String[] args) {
 
+        int correctMathQuestions = 0;
+
         
         // to set and create names for the player
         
-        System.out.printf("Welcome! Let's Play Some Trivia!\n\n What is your name?: "); //normal text
+        System.out.printf("\n\nWelcome! Let's Play Some Trivia!\n\n What is your name?: "); //normal text
 
         Scanner console = new Scanner(System.in); // Scanner to take information from users
 
@@ -34,15 +36,32 @@ public class Main{
             System.out.print("\n[Please enter command or type: ? for the Menu]: ");
             choice = console.next().toLowerCase();
             command = choice.charAt(0);
+            System.out.print("\n");
 
             switch (command){
 
                 case 'm':
-                    // start of math questions! 
+                    // start of math questions!
+                    
                     System.out.printf("\nNice choice! %s, I'm a big fan of math!\n\n", player.getName());
                     Math mathQuest = new Math();
                     int randomQuestion = mathQuest.randomNum();
-                    System.out.print(mathQuest.questions((randomQuestion)));
+                    System.out.print("\n" + mathQuest.questions(randomQuestion)); // print out question
+                    int answer = console.nextInt();
+                    if (mathQuest.checkAnswer(randomQuestion, answer)){
+                        if (correctMathQuestions >= 5){ 
+                            System.out.printf("\nNice Job! You got 5 right!");
+                            break;
+                        }
+                        correctMathQuestions++;
+                        System.out.printf("\nCorrect! You have %d right! %d more to go!\n", correctMathQuestions, 5 - correctMathQuestions);
+                        
+                    }
+                    else { 
+                        System.out.println("\nDarn, thats not quite it");
+                    }
+
+                    break;
                     
 
                 case '?': 
@@ -53,16 +72,28 @@ public class Main{
                 case 'q': // statement to end the program
                     break;
 
+                default:
+                    System.out.printf("\nSorry %s, %s is not a valid command\n", player.getName(), command);
+                    break;
+
+
             }
 
 
-        } while (command != 'q');
+        } while (command != 'q' && correctMathQuestions != 5);
+
+        if (correctMathQuestions == 5){
+            System.out.printf("\nCongratulations! You got 5 math questions right\n\n");
+        }
+        else { System.out.printf("\nGame over..."); }
 
 
         console.close(); // used to close console input
 
 
     } // end of main method
+
+    
 
     // start of print menu method
     public static void printMenu(){
