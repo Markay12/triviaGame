b26c58fc5eb40892197ecd59a5075c7d8201145e
Markay12 <54555076+Markay12@users.noreply.github.com>
2019-12-22 12:10:50 -0700

    Finished method with questions and answers

--- Programming.java
+++ Programming.java
@@ -14,7 +14,7 @@ public class Programming{
 
     public int programRandomNum(){
 
-        int questionsSize = 2;
+        int questionsSize = 10;
 
         Random rand = new Random();
 
@@ -34,7 +34,31 @@ public class Programming{
         else if (questionNum == 2){
             return "Was the Programming language Python influenced by the Monty Python series?: ";
         }
-        else { return ""; }
+        else if (questionNum == 3){
+            return "In the programming language Python. Is 'elseif' the correct syntax use for an else if statement?: ";
+        }
+        else if (questionNum == 4){
+            return "Does the 'q' in QBasic stand for 'Quick'?: ";
+        }
+        else if (questionNum == 5){
+            return "Is a standard cd 10 cm in length?: ";
+        }
+        else if (questionNum == 6){
+            return "Does the programming language Python use Arrays?: ";
+        }
+        else if (questionNum == 7){
+            return "When doing data analysis would using the language SQL be a good fit?: ";
+        }
+        else if (questionNum == 8){
+            return "Would 'total_in_$' be an INVALID identifier?: ";
+        }
+        else if (questionNum == 9){
+            return "If I wanted to add a new row into an SQL database would 'INSERT' be the correct syntax?: ";
+        }
+        else if (questionNum == 10){
+            return "Viruses, trojans and worms are bad, Yes or No?: ";
+        }
+        else { return "[Invalid Number - Check method programRandomNum in Programming.java]"; }
 
     }
 
@@ -47,6 +71,54 @@ public class Programming{
             else { return false; }
         }
         if (questionNum == 2){
+            if (answer == 1){
+                return true;
+            }
+            else { return false; }
+        }
+        if (questionNum == 3){
+            if (answer == 2){
+                return true;
+            }
+            else { return false; }
+        }
+        if (questionNum == 4){
+            if (answer == 1){
+                return true;
+            }
+            else { return false; }
+        }
+        if (questionNum == 5){
+            if (answer == 2){
+                return true;
+            }
+            else { return false; }
+        }
+        if (questionNum == 6){
+            if(answer == 2){
+                return true;
+            }
+            else { return false; }
+        }
+        if (questionNum == 7){
+            if (answer == 1){
+                return true;
+            }
+            else { return false; }
+        }
+        if (questionNum == 8){
+            if (answer == 1){
+                return true;
+            }
+            else { return false; }
+        }
+        if (questionNum == 9){
+            if (answer == 1){
+                return true;
+            }
+            else { return false; }
+        }
+        if (questionNum == 10){
             if (answer == 1){
                 return true;
             }
